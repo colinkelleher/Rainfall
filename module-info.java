fd9008fbcdde0cf60650a4cff4ca7cf07955bf0e
Colin Kelleher <43775947+colinkelleher@users.noreply.github.com>
2020-04-16 19:23:44 +0100

    module-info.java

---
+++ module-info.java
@@ -0,0 +1,20 @@
+/**
+ * <h1>Rainfall Module</h1>
+ * <p>This <b>Rainfall</b> module is used to predict Rainfall levels at Cork Airport based
+ * on previous rainfall recordings</p>
+ *
+ * <p>This module contains the following packages</p>
+ * <ul>
+ *  <li>Application - This package contains Java Classes to run the overall program </li>
+ *  <li>DataStore - This package contains Java Classes used for handling data from the data source </li>
+ *  <li>Exceptions - This package holds the exceptions </li>
+ *  <li>Model - This package contains Java Classes used for prediction </li>
+ *  </ul>
+ *
+ *  @author Colin Kelleher
+ *  @since 2019-10-30
+ *  @version 1.0
+ *
+ */
+module Rainfall {
+}
